a02cf5e5c7539890f9131546c5b512ccf3b4125d
jakobschulz <47540308+jakobschulz@users.noreply.github.com>
2019-03-04 11:41:11 +1300

    Add files via upload

---
+++ untitled-1.py
@@ -0,0 +1,36 @@
+#Comic book store website.
+#Cosmo's Comics
+
+from bottle import run, route, view, get, post, request
+from itertools import count
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+run(host ='0.0.0.0', port = 8080, reloader = True, debug = True)
